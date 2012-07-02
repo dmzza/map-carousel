af5899be1e8ac904b2937117363c4b17b4ad7783
David Mazza <dmazza90@gmail.com>
2012-07-01 20:30:01 -0400

    Initial Commit
    Grunt generated project files and directory

---
+++ src/map-carousel.js
@@ -0,0 +1,28 @@
+/*
+ * map-carousel
+ * https://github.com/dmazza/map-carousel
+ *
+ * Copyright (c) 2012 David Mazza
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
